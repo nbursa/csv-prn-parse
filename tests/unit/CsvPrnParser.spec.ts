b0e91141585f92e79f5b676698ce806d7327f4ee
Nenad <nbursa@gmail.com>
2022-03-14 23:20:01 +0100

    deploy

--- tests/unit/CsvPrnParser.spec.ts
+++ tests/unit/CsvPrnParser.spec.ts
@@ -5,8 +5,20 @@ import { enableFetchMocks } from 'jest-fetch-mock'
 enableFetchMocks()
 
 describe('CsvPrnParser.vue', () => {
-  it('renders document data', () => {
+  it('renders csv data', () => {
     const wrapper = mount(CsvPrnParser)
     expect(wrapper.get('.data-table').isVisible()).toBe(true)
+    expect(wrapper.get('.toggle button').text()).toContain('Show JSON')
+  })
+  it('renders prn data', () => {
+    const wrapper = mount(CsvPrnParser, {
+      data () {
+        return {
+          fileFormat: 'prn'
+        }
+      }
+    })
+    expect(wrapper.get('.data-table').isVisible()).toBe(true)
+    expect(wrapper.get('.toggle button').text()).toContain('Show JSON')
   })
 })
